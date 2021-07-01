ecc75d167ddcb94a8b3cd08453e651e937e10857
Anton Chaiko <a.chaiko.vironit@vironit.ru>
2021-07-01 23:58:28 +0300

    ref: implement Coil

--- app/src/main/java/com/example/wallpapersapp/MainActivity.kt
+++ app/src/main/java/com/example/wallpapersapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wallpapersapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
