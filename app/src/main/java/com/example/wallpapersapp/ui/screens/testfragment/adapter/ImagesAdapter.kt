9e2d2453edf17f8e3019006ad4f7f5786e89eebf
Anton Chaiko <a.chaiko.vironit@vironit.ru>
2021-07-08 16:42:25 +0300

    ref: rename refs

--- app/src/main/java/com/example/wallpapersapp/ui/screens/testfragment/adapter/ImagesAdapter.kt
+++ app/src/main/java/com/example/wallpapersapp/ui/screens/testfragment/adapter/ImagesAdapter.kt
@@ -1,8 +1,6 @@
 package com.example.wallpapersapp.ui.screens.testfragment.adapter
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,31 +8,30 @@ import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import coil.transition.Transition
 import com.example.data.model.Results
 import com.example.wallpapersapp.R
 import com.example.wallpapersapp.databinding.GridItemViewBinding
 
 class ImagesAdapter(context: Context) :
-    PagingDataAdapter<Results, HomeNewsViewHolder>(ArticleDiffItemCallback) {
+    PagingDataAdapter<Results, ImagesViewHolder>(ArticleDiffItemCallback) {
 
     private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeNewsViewHolder {
-        return HomeNewsViewHolder(layoutInflater.inflate(R.layout.grid_item_view, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImagesViewHolder {
+        return ImagesViewHolder(layoutInflater.inflate(R.layout.grid_item_view, parent, false))
     }
 
-    override fun onBindViewHolder(holder: HomeNewsViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ImagesViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 }
 
-class HomeNewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ImagesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val binding = GridItemViewBinding.bind(itemView)
 
-    fun bind(article: Results?) = with(itemView) {
-        binding.marsImage.load(article?.urls?.regular){
+    fun bind(images: Results?) = with(itemView) {
+        binding.marsImage.load(images?.urls?.regular){
 //            placeholder(ColorDrawable(Color.TRANSPARENT))
             crossfade(true)
 //            crossfade(1000)
